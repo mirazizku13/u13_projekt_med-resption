fd91425683edde85fb4d1603106c99ac7efaeb48
mirazizku13 <mirazizmirabdulayev11@gmail.com>
2026-06-15 18:06:04 +0500

    doctors model

---
+++ accounts/serializers.py
@@ -0,0 +1,19 @@
+from accounts.models import User
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            'username',
+            'email',
+            'first_name',
+            'last_name'
+        ]
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.core.serializers import serialize
+from rest_framework.decorators import action
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+
+from accounts.serializers import LoginSerializer, UserSerializer
+
 
 # Create your views here.
+class AuthViewSet(viewsets.ViewSet):
+    @action(methods=['post'], detail=False, url_path='login')
+    def login_user(self, request):
+        serialize = LoginSerializer(request.data)
+        if serialize.is_valid():
+            username = serialize.data['username']
+            password = serialize.data['password']
+            user = authenticate(username=username, password=password)
+
+            if user:
+                login(request, user)
+                return Response(UserSerializer(user).data, status=status.HTTP_200_OK)
+            return Response({'message': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
