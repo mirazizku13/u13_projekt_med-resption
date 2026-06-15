b6ed8b0bbd68cc5e28e3a4838ed3a9e02c83f741
mirazizku13 <mirazizmirabdulayev11@gmail.com>
2026-06-15 17:00:12 +0500

    first commit

---
+++ accounts/models.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    phone_number = models.CharField(max_length=11)
+
+    class Meta:
+        db_table = 'users'
